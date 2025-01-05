24912b1f10cf802f72199d2bc8bd4bdb0e1bb422
MoRoPoPi <omarmandri26@gmail.com>
2025-01-05 13:51:15 +0100

    refactor: update data handling in Page component and improve CardList interface

--- types/index.ts
+++ types/index.ts
@@ -66,3 +66,40 @@ export interface SearchManuFacturerProps {
   manufacturer: string;
   setManuFacturer: (manufacturer: string) => void;
 }
+
+
+export type UserType = "GEST_ENTRE" | "RESPO_STAGE" | "ADMIN_ECOLE" | "ETUDIANT" | "PROFESSEUR" | "TUTEUR"; // Add more user types as needed
+export type StageStatutType = "EN_ATTENTE" | "ACCEPTE" | "REFUSE" | "EXPIRE";
+
+export interface User {
+  idUser: number;
+  nom: string;
+  prenom: string;
+  email: string;
+  tel: string | null;
+  userType: UserType;
+}
+
+export interface Entreprise {
+  idEntreprise: number;
+  nomEntreprise: string;
+  adresse: string;
+  tel: string;
+}
+
+export interface Gestionnaire {
+  idGestEntr: number;
+  user: User;
+  entreprise: Entreprise;
+}
+
+export interface Stage {
+  idStage: number;
+  gestionnaire: Gestionnaire;
+  titre: string;
+  description: string;
+  dateDebut: string; // ISO 8601 format
+  dateFin: string; // ISO 8601 format
+  statut: StageStatutType;
+  abbreviation: "PFA_1A" | "PFA_2A" | "PFE";
+}
